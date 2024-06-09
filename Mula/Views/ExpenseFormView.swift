68cb44f926caf1ae38439e67997383d16b6e76f5
Shanti Mickens <shanti.mickens@gmail.com>
2024-06-09 16:38:59 -0700

    added save option to edit expense form

--- Mula/Views/ExpenseFormView.swift
+++ Mula/Views/ExpenseFormView.swift
@@ -11,7 +11,6 @@ import SwiftUI
 
 struct NewExpenseFormView: View {
     @Environment(\.modelContext) private var modelContext
-    @Environment(\.dismiss) private var dismiss
     
     @State private var title: String = ""
     @State private var date: Date = Date()
@@ -19,45 +18,42 @@ struct NewExpenseFormView: View {
     @State private var category: Category = .misc
 
     var body: some View {
-        ExpenseFormView(title: $title, date: $date, amount: $amount, category: $category)
-            .toolbar {
-                ToolbarItem(placement: .confirmationAction) {
-                    Button{
-                        saveExpense()
-                    } label: {
-                        Text("Save Expense")
-                    }
-                    .disabled(!isFormValid)
-                }
-            }
+        ExpenseFormView(title: $title, date: $date, amount: $amount, category: $category, save: save)
     }
 
-    private var isFormValid: Bool {
-        return title.trimmingCharacters(in: .whitespaces).count > 0 && amount > 0
-    }
-
-    private func saveExpense() {
+    private func save() {
         // Handle saving the new expense, for example, you could add it to an array or store it in a database.
         let expenseAmount = category == .income ? amount : -amount
-        
         let newExpense = Expense(title: title, date: date, amount: expenseAmount, category: category)
         modelContext.insert(newExpense)
-
-        // Reset the form after saving the expense
-        title = ""
-        date = Date()
-        amount = 0.0
-        category = .misc
-
-        dismiss()
     }
 }
 
 struct EditExpenseFormView: View {
     @Bindable var expense: Expense
     
+    @State private var title: String
+    @State private var date: Date
+    @State private var amount: Double
+    @State private var category: Category
+    
+    init(expense: Expense) {
+        self.expense = expense
+        _title = State(initialValue: expense.title)
+        _date = State(initialValue: expense.date)
+        _amount = State(initialValue: expense.amount)
+        _category = State(initialValue: expense.category)
+    }
+    
     var body: some View {
-        ExpenseFormView(title: $expense.title, date: $expense.date, amount: $expense.amount, category: $expense.category)
+        ExpenseFormView(title: $title, date: $date, amount: $amount, category: $category, save: save)
+    }
+    
+    func save() {
+        expense.title = title
+        expense.date = date
+        expense.amount = amount
+        expense.category = category
     }
 }
 
@@ -67,6 +63,8 @@ struct ExpenseFormView: View {
     @Binding var date: Date
     @Binding var amount: Double
     @Binding var category: Category
+    
+    let save: (() -> Void)
 
     var body: some View {
         VStack {
@@ -106,9 +104,20 @@ struct ExpenseFormView: View {
                 .foregroundColor(.red)
             }
         }
+        .toolbar {
+            ToolbarItem(placement: .confirmationAction) {
+                Button{
+                    save()
+                    dismiss()
+                } label: {
+                    Text("Save")
+                }
+                .disabled(!isFormValid)
+            }
+        }
     }
 
     private var isFormValid: Bool {
-        return title.trimmingCharacters(in: .whitespaces).count > 0 && amount > 0
+        return title.trimmingCharacters(in: .whitespaces).count > 0 && amount != 0
     }
 }
