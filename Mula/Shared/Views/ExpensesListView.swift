01885121bf8f5e258fb6b12ecaa476232fcc6b4b
Shanti Mickens <shanti.mickens@gmail.com>
2024-07-20 18:24:46 -0700

    Used navigation path to have a detail transaction view

--- Mula/Shared/Views/ExpensesListView.swift
+++ Mula/Shared/Views/ExpensesListView.swift
@@ -8,18 +8,41 @@
 import SwiftUI
 
 struct ExpensesListView: View {
+    @Binding var navigationPath: NavigationPath
+    @Binding var selectedMonth: String
+
     @State private var searchText: String = ""
-    @State private var selectedMonth: String = Date().month
     @State private var expenses: [Expense] = []
 
     var body: some View {
         VStack {
             HeaderView(title: "Expenses", selectedMonth: $selectedMonth)
+                .padding()
 
             List(expenses) { expense in
-                ExpenseView(expense: expense, swipeActionsEnabled: true)
+                TransactionView(expense: expense)
+                    .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                        Button {
+                            navigationPath.append(expense)
+                        } label: {
+                            Label("Edit", systemImage: "pencil")
+                        }
+                        .tint(.yellow)
+                    }.swipeActions(edge: .trailing, allowsFullSwipe: false) {
+                        Button(role: .destructive) {
+                            print("delete item: \(expense.id) w/ title \(expense.title)")
+                            // TODO: DataManager.delete(expense.id)
+                        } label: {
+                            Label("Delete", systemImage: "trash.fill")
+                        }
+                    }
             }
-            .searchable(text: $searchText)
+            .listStyle(.plain)
+            .listItemTint(Color(.systemGray6))
+        }
+        // TODO: might move up to the contentview layer
+        .onAppear {
+            expenses = DataManager.shared.expenses(for: selectedMonth)
         }
         .onChange(of: selectedMonth) { _, newValue in
             expenses = DataManager.shared.expenses(for: newValue)
@@ -27,6 +50,6 @@ struct ExpensesListView: View {
     }
 }
 
-#Preview {
-    ExpensesListView()
-}
+//#Preview {
+//    ExpensesListView(selectedMonth: .constant("May"))
+//}
