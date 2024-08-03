35238fc87c13c2f8d31f18551ef8b37a7674e135
Shanti Mickens <shanti.mickens@gmail.com>
2024-08-03 13:49:22 -0700

    Added filtering by bucket or category

--- Mula/iOS/Views/ExpensesListView.swift
+++ Mula/iOS/Views/ExpensesListView.swift
@@ -10,22 +10,32 @@ import SwiftUI
 struct ExpensesListView: View {
     @Bindable var dataManager: DataManager
     @State private var selectedExpense: Expense? = nil
-    @State private var addingNewTransaction: Bool = false
+    @State private var addingNewExpense: Bool = false
+    @State private var filteringExpenses: Bool = false
     @State private var newExpense = Expense(id: nil, title: "", date: Date(), amount: 0.0, bucket: .spending, category: .eatingOut)
     @State private var searchText: String = ""
+    @State private var activeBucketFilters: Set<Bucket> = []
+    @State private var activeCategoryFilters: Set<Category> = []
 
     var body: some View {
         VStack(spacing: 0) {
             HeaderView(title: "Expenses", selectedMonth: $dataManager.selectedMonth)
                 .padding()
 
-            HStack {
+            HStack(spacing: 0) {
                 SearchBarView(searchText: $searchText)
 
-                Spacer(minLength: 0)
+                Button {
+                    filteringExpenses.toggle()
+                } label: {
+                    Image(systemName: filterIsActive ? "line.3.horizontal.decrease.circle.fill" : "line.3.horizontal.decrease.circle")
+                        .imageScale(.large)
+                        .tint(.indigo)
+                }
+                .padding(.trailing)
 
                 Button {
-                    addingNewTransaction.toggle()
+                    addingNewExpense.toggle()
                 } label: {
                     Image(systemName: "plus.square.fill")
                         .imageScale(.large)
@@ -35,7 +45,7 @@ struct ExpensesListView: View {
             }
             .padding(.top, -15)
 
-            List(searchResults, id: \.id) { expense in
+            List(filteredSearchResults, id: \.id) { expense in
                 ExpenseView(expense: expense)
                     .expenseSwipeActions {
                         selectedExpense = expense
@@ -51,12 +61,16 @@ struct ExpensesListView: View {
             .listStyle(.plain)
             .listItemTint(Color(.systemGray6))
         }
-        .sheet(isPresented: $addingNewTransaction) {
+        .sheet(isPresented: $addingNewExpense) {
             ExpenseAddView(expense: newExpense)
                 .presentationDetents([.height(500)])
         }
         .sheet(item: $selectedExpense) { expense in
             ExpenseEditView(expense: expense)
+                .presentationDetents([.height(500)])
+        }
+        .sheet(isPresented: $filteringExpenses) {
+            filterView
                 .presentationDetents([.height(500)])
         }
     }
@@ -69,5 +83,92 @@ struct ExpensesListView: View {
         }
 
         return expenses.filter { $0.title.lowercased().contains(searchText.lowercased()) }
+    }
+
+    var filteredSearchResults: [Expense] {
+        guard filterIsActive else {
+            return searchResults
+        }
+        return searchResults.filter {
+            activeBucketFilters.contains($0.bucket) || activeCategoryFilters.contains($0.category)
+        }
+    }
+
+    var filterIsActive: Bool {
+        return !activeBucketFilters.isEmpty || !activeCategoryFilters.isEmpty
+    }
+
+    var filterView: some View {
+        VStack {
+            Form {
+                Section(header: Text("Select Filters").font(.headline)) {
+                    ForEach(Bucket.allCases, id: \.self) { bucket in
+                        let isSelected = activeBucketFilters.contains(bucket)
+
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .fill(bucket.tint)
+                                    .frame(width: 25, height: 25)
+
+                                Image(systemName: bucket.icon)
+                                    .foregroundColor(.white)
+                                    .imageScale(.small)
+                            }
+
+                            Text(bucket.name)
+                                .font(.body)
+                                .fontWeight(isSelected ? .semibold : .regular)
+
+                            Spacer()
+
+                            Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
+                                .foregroundColor(isSelected ? bucket.tint : .gray)
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            if isSelected {
+                                activeBucketFilters.remove(bucket)
+                            } else {
+                                activeBucketFilters.insert(bucket)
+                            }
+                        }
+                    }
+
+                    ForEach(Category.allCases, id: \.self) { category in
+                        let isSelected = activeCategoryFilters.contains(category)
+
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .fill(category.tintColor)
+                                    .frame(width: 25, height: 25)
+
+                                Image(systemName: category.iconName)
+                                    .foregroundColor(.white)
+                                    .imageScale(.small)
+                            }
+
+                            Text(category.name)
+                                .font(.body)
+                                .fontWeight(isSelected ? .semibold : .regular)
+
+                            Spacer()
+
+                            Image(systemName: isSelected ? "checkmark.circle.fill" : "circle")
+                                .foregroundColor(isSelected ? category.tintColor : .gray)
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            if isSelected {
+                                activeCategoryFilters.remove(category)
+                            } else {
+                                activeCategoryFilters.insert(category)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
