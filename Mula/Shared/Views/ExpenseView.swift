19532f150525e8b3076da82c3ada540462ef6fdf
Shanti Mickens <shanti.mickens@gmail.com>
2025-10-04 16:02:40 -0700

    comment out unused file

--- Mula/Shared/Views/ExpenseView.swift
+++ Mula/Shared/Views/ExpenseView.swift
@@ -5,52 +5,52 @@
 //  Created by Shanti Mickens on 2/1/24.
 //
 
-import SwiftUI
-
-struct ExpenseView: View {
-    @Bindable var expense: Expense
-
-    var body: some View {
-        HStack {
-            ZStack {
-                Circle()
-                    .fill(expense.category.tintColor)
-                    .frame(width: 35, height: 35)
-
-                Image(systemName: expense.category.iconName)
-                    .foregroundColor(.white)
-            }
-
-            VStack(alignment: .leading, spacing: 5) {
-                Text(expense.title)
-                    .font(.headline)
-                    .lineLimit(1)
-
-                HStack(spacing: 5) {
-                    Text(expense.bucket.name)
-                        .font(.caption)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(Color(.systemGray))
-                        .padding(.vertical, 2)
-                        .padding(.horizontal, 4)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(3.0)
-                        .padding(0)
-
-                    Text(formatDate(expense.date))
-                        .font(.caption)
-                }
-            }
-
-            Spacer()
-
-            ExpenseAmountView(amount: expense.amount)
-        }
-    }
-
-    private func formatDate(_ date: Date?) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE, MMM d"
-        return dateFormatter.string(from: date ?? Date())
-    }
-}
+//import SwiftUI
+//
+//struct ExpenseView: View {
+//    @Bindable var expense: Expense
+//
+//    var body: some View {
+//        HStack {
+//            ZStack {
+//                Circle()
+//                    .fill(expense.category.tintColor)
+//                    .frame(width: 35, height: 35)
+//
+//                Image(systemName: expense.category.iconName)
+//                    .foregroundColor(.white)
+//            }
+//
+//            VStack(alignment: .leading, spacing: 5) {
+//                Text(expense.title)
+//                    .font(.headline)
+//                    .lineLimit(1)
+//
+//                HStack(spacing: 5) {
+//                    Text(expense.bucket.name)
+//                        .font(.caption)
+//                        .fontWeight(.semibold)
+//                        .foregroundStyle(Color(.systemGray))
+//                        .padding(.vertical, 2)
+//                        .padding(.horizontal, 4)
+//                        .background(Color(.systemGray6))
+//                        .cornerRadius(3.0)
+//                        .padding(0)
+//
+//                    Text(formatDate(expense.date))
+//                        .font(.caption)
+//                }
+//            }
+//
+//            Spacer()
+//
+//            ExpenseAmountView(amount: expense.amount)
+//        }
+//    }
+//
+//    private func formatDate(_ date: Date?) -> String {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "EEEE, MMM d"
+//        return dateFormatter.string(from: date ?? Date())
+//    }
+//}
