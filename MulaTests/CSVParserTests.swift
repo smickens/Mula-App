ff4f2529cc6c700c1df0b10812165b5e98e41f8d
Shanti Mickens <shanti.mickens@gmail.com>
2026-06-04 19:53:25 -0700

    Add a Mula Tests target with a starter test

---
+++ MulaTests/CSVParserTests.swift
@@ -0,0 +1,15 @@
+//
+//  CSVParserTests.swift
+//  MulaTests
+//
+//  Created by Shanti Mickens on 6/4/26.
+//
+
+import XCTest
+@testable import Mula
+
+final class CSVParserTests: XCTestCase {
+    func testPlaceholder() {
+        XCTAssertTrue(true)
+    }
+}
